6f140e5a76166ea68ed65a2614a1fada3252af45
Sumit Kumar <sumit.sh25@yahoo.com>
2019-02-22 20:32:13 +0530

    test

---
+++ OutlookREST/app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from app import views
+
+urlpatterns = [
+  # /tutorial
+  path('', views.home, name='home'),
+  path('signin', views.sign_in, name='signin'),
+  path('signout', views.sign_out, name='signout'),
+  path('callback', views.callback, name='callback'),
+  path('calendar', views.calendar, name='calendar'),
+]
